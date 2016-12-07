f562f335944c258e613f3c62d0667ef3357a2a3b
Gurvv29 <Gurvan.chevallier@gmail.com>
2016-12-07 18:01:47 +0100

    Create Load.java

---
+++ Load.java
@@ -0,0 +1,13 @@
+package mrdragonn.gurvv29.Textual_rpg;
+
+public class Load {
+
+	public static void Loading(){
+		System.out.println("## LOADING ##");
+		System.exit(0);
+	}
+	public static void NewGame(){
+		System.out.println("## || NEW GAME || ##");
+		System.exit(0);
+	}
+}
